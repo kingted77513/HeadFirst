512ff59e3b4f386cfcb3fdfbcdafd4acd6eb9d67
kingt <kingt@TeddyLaptop>
2021-07-08 13:44:58 +0800

    實作CheckYourselfWhenKill程式碼。 並且新增hit、miss、kill測試案例。

--- Java/src/chap05/SimpleDotCom.java
+++ Java/src/chap05/SimpleDotCom.java
@@ -4,8 +4,22 @@ public class SimpleDotCom {
 	private int[] locationCells;
 	int numOfHits;
 	
-	String checkYourself(String guess) {
-		return null;
+	String checkYourself(String stringGuess) {
+		int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for (int cell: locationCells)
+        {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length)
+        {
+            result = "kill";
+        }
+        return result;
 	}
 	
 	void setLocationCells(int[] loc) {
--- Java/src/chap05/SimpleDotComTest.java
+++ Java/src/chap05/SimpleDotComTest.java
@@ -7,13 +7,39 @@ import org.junit.jupiter.api.Test;
 class SimpleDotComTest {
 
 	@Test
-	void testCheckYourself() {
+	void testCheckYourselfWhenHit() {
 		SimpleDotCom dot = new SimpleDotCom();
 		int[] locations = {2, 3, 4};
         dot.setLocationCells(locations);
         
         String userGuess = "2";
         String excepted = "hit";
+        String actual = dot.checkYourself(userGuess);
+        
+        assertEquals(excepted, actual);
+	}
+	
+	@Test
+	void testCheckYourselfWhenMiss() {
+		SimpleDotCom dot = new SimpleDotCom();
+		int[] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+        
+        String userGuess = "5";
+        String excepted = "miss";
+        String actual = dot.checkYourself(userGuess);
+        
+        assertEquals(excepted, actual);
+	}
+	
+	@Test
+	void testCheckYourselfWhenKill() {
+		SimpleDotCom dot = new SimpleDotCom();
+		int[] locations = {2};
+        dot.setLocationCells(locations);
+        
+        String userGuess = "2";
+        String excepted = "kill";
         String actual = dot.checkYourself(userGuess);
         
         assertEquals(excepted, actual);
