20bb105eca82c5f87cd9e98762589e9b39aecfb9
kingt <kingt@TeddyLaptop>
2021-07-08 12:37:14 +0800

    新增SimpleDotCom，建立基本架構

---
+++ Java/src/chap05/SimpleDotCom.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class SimpleDotCom {
+	private int[] locationCells;
+	int numOfHits;
+	
+	String checkYourself(String guess) {
+		return null;
+	}
+	
+	void setLocationCells(int[] loc) {
+		
+	}
+
+}
