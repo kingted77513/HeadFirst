7d3aefb00be5d1284b9a81b0baf588cb70b7e711
kingt <kingt@TeddyLaptop>
2021-07-08 09:42:12 +0800

    增加練習程式

---
+++ Java/src/chap02/DrumKit.java
@@ -0,0 +1,16 @@
+package chap02;
+
+public class DrumKit {
+	
+	boolean topHat = true;
+	boolean snare = true;
+	
+	void playSnare() {
+		System.out.println("bang bang ba-bang");
+	}
+	
+	void playTopHat() {
+		System.out.println("ding ding ba-ding");
+	}
+
+}
