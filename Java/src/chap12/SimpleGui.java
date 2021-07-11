ff52ad87acc5dbf2813c13ddd449061b4699cdb9
kingt <kingt@TeddyLaptop>
2021-07-11 16:40:40 +0800

    建立Listener並且處理事件

--- Java/src/chap12/SimpleGui.java
+++ Java/src/chap12/SimpleGui.java
@@ -1,8 +1,13 @@
 package chap12;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class SimpleGui {
+public class SimpleGui implements ActionListener{
+	JButton button;
+	
 	public static void main (String[] args) {
 		SimpleGui gui = new SimpleGui();
 		gui.go();
@@ -10,11 +15,19 @@ public class SimpleGui {
 
 	private void go() {
 		JFrame frame = new JFrame();
-		JButton button = new JButton("click me");
+		button = new JButton("click me");
+		
+		button.addActionListener(this);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(button);
 		frame.setSize(300, 300);
 		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		System.out.println(event.getActionCommand());
+		button.setText("I've benn clicked!");
 	}
 }
