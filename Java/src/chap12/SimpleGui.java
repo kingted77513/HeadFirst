8bb12bcb1ce5b8607ec200cadc0311611ba66334
kingt <kingt@TeddyLaptop>
2021-07-11 17:30:29 +0800

    修改成按下按鈕時改變顏色

--- Java/src/chap12/SimpleGui.java
+++ Java/src/chap12/SimpleGui.java
@@ -1,12 +1,12 @@
 package chap12;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class SimpleGui implements ActionListener{
-	JButton button;
+	
+	JFrame frame;
 	
 	public static void main (String[] args) {
 		SimpleGui gui = new SimpleGui();
@@ -14,13 +14,15 @@ public class SimpleGui implements ActionListener{
 	}
 
 	private void go() {
-		JFrame frame = new JFrame();
-		button = new JButton("click me");
-		
+		frame = new JFrame();
+		JButton button = new JButton("Change colors");
 		button.addActionListener(this);
 		
+		MyDrawPanel drawPanel = new MyDrawPanel();
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(button);
+		frame.getContentPane().add(BorderLayout.SOUTH, button);
+		frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
@@ -28,6 +30,31 @@ public class SimpleGui implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		System.out.println(event.getActionCommand());
-		button.setText("I've benn clicked!");
+		frame.repaint();
+	}
+	
+	@SuppressWarnings("serial")
+	private class MyDrawPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			g.setColor(Color.WHITE);
+			g.fillRect(10, 10, this.getWidth() - 20, this.getHeight() - 20);
+			
+			Color randomColor = getRandomColor();
+			g.setColor(randomColor);
+			g.fillRect(70, 70, 100, 100);
+		}
+
+		private Color getRandomColor() {
+			int red = getRandomIntergerUnder256();
+			int green = getRandomIntergerUnder256();
+			int blue = getRandomIntergerUnder256();
+			
+			Color randomColor = new Color(red, green, blue);
+			return randomColor;
+		}
+		
+		private int getRandomIntergerUnder256() {
+			return (int) (Math.random() * 256);
+		}
 	}
 }
