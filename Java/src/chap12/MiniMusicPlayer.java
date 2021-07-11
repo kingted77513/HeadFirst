efc46e531aa0ea661ba06d13179c72895b995148
kingt <kingt@TeddyLaptop>
2021-07-11 17:44:16 +0800

    匯入範例程式

---
+++ Java/src/chap12/MiniMusicPlayer.java
@@ -0,0 +1,46 @@
+package chap12;
+import javax.sound.midi.*;
+
+// this one plays random music with it, but only because there is a listener.
+
+public class MiniMusicPlayer {
+
+	public static void main(String[] args) {
+		try {
+
+			// make (and open) a sequencer, make a sequence and track
+
+			Sequencer sequencer = MidiSystem.getSequencer();         
+			sequencer.open();
+
+			Sequence seq = new Sequence(Sequence.PPQ, 4);
+			Track track = seq.createTrack();     
+
+			// now make two midi events (containing a midi message)
+
+			for (int i = 5; i < 61; i+= 4) {
+
+				track.add(makeEvent(144,1,i,100,i));
+				track.add(makeEvent(128,1,i,100,i + 2));
+			} // end loop
+
+			// add the events to the track            
+			// add the sequence to the sequencer, set timing, and start
+
+			sequencer.setSequence(seq);
+			sequencer.setTempoInBPM(220);
+			sequencer.start();
+		} catch (Exception ex) {ex.printStackTrace();}
+	}
+
+	private static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+		MidiEvent event = null;
+		try {
+			ShortMessage a = new ShortMessage();
+			a.setMessage(comd, chan, one, two);
+			event = new MidiEvent(a, tick);
+		}catch(Exception e) { }
+		return event;
+	}
+
+} // close class
