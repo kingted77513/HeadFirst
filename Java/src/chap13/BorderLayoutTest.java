5e717a971249c5dd5428afc6d941d4eb3c137c1a
kingt <kingt@TeddyLaptop>
2021-07-12 20:02:19 +0800

    新增一個字體較大的北邊組件，每個部份只能放一個組件，並且高度自動拉長。

--- Java/src/chap13/BorderLayoutTest.java
+++ Java/src/chap13/BorderLayoutTest.java
@@ -16,6 +16,8 @@ public class BorderLayoutTest {
 		JButton east_LongName = new JButton("Long East");
 		JButton west = new JButton("West");
 		JButton north = new JButton("North");
+		JButton north_BigSize = new JButton("Big North");
+		north_BigSize.setFont(new Font("serif", Font.BOLD, 28));
 		JButton south = new JButton("South");
 		JButton center = new JButton("Center");
 		
@@ -24,6 +26,7 @@ public class BorderLayoutTest {
 		cp.add(BorderLayout.EAST, east_LongName);
 		cp.add(BorderLayout.WEST, west);
 		cp.add(BorderLayout.NORTH, north);
+		cp.add(BorderLayout.NORTH, north_BigSize);
 		cp.add(BorderLayout.SOUTH, south);
 		cp.add(BorderLayout.CENTER, center);
 		
