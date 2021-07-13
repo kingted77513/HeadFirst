889045e8c4b4b4cf5585a06773f0fa25ee62a39c
kingt <kingt@TeddyLaptop>
2021-07-13 09:29:26 +0800

    新增一個Button，寬度配合Butoon拉寬。

--- Java/src/chap13/FlowLayoutTest.java
+++ Java/src/chap13/FlowLayoutTest.java
@@ -14,6 +14,11 @@ public class FlowLayoutTest {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
+		
+		JButton button = new JButton("Button");
+		
+		panel.add(button);
+		
 		frame.getContentPane().add(BorderLayout.EAST, panel);
 		frame.setSize(200, 200);
 		frame.setVisible(true);
