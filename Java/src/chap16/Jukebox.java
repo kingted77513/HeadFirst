e3adcbad4247697331b8797cebf2fb97b2df3250
kingt <kingt@TeddyLaptop>
2021-07-15 17:21:52 +0800

    新增以歌手名字為排序的Comparator

--- Java/src/chap16/Jukebox.java
+++ Java/src/chap16/Jukebox.java
@@ -10,11 +10,23 @@ public class Jukebox
     public static void main(String[] args) {
         new Jukebox().go();
     }
+
+    class ArtistCompare implements Comparator<Song> {
+        public int compare(Song one, Song two) {
+            return one.getArtist().compareTo(two.getArtist());
+        }
+    }
     
     public void go() {
         getSongs();
         System.out.println(songList);
+        
         Collections.sort(songList);
+        System.out.println(songList);
+        
+        ArtistCompare artistCompare = new ArtistCompare();
+        Collections.sort(songList, artistCompare);
+        
         System.out.println(songList);
     }
     
