5e9a16b7805be2441d8748acfc47ce59ec9c3d38
kingt <kingt@TeddyLaptop>
2021-07-15 16:00:17 +0800

    匯入範例程式碼

---
+++ Java/src/chap16/Jukebox.java
@@ -0,0 +1,35 @@
+package chap16;
+
+import java.util.*;
+import java.io.*;
+
+public class Jukebox
+{
+    ArrayList<String> songList = new ArrayList<String>();
+    
+    public static void main(String[] args) {
+        new Jukebox().go();
+    }
+    
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+    }
+    
+    void getSongs() {
+        try {
+            File file = new File("SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+            reader.close();
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+    
+    void addSong(String lineToParse) {
+        String[]tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
+}
